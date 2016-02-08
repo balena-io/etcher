fedealconada/resin-etcher

Implement OS X installer

Makefile
@@ -1,9 +1,11 @@
 ELECTRON_PACKAGER=./node_modules/.bin/electron-packager
+ELECTRON_BUILDER=./node_modules/.bin/electron-builder
 ELECTRON_IGNORE=$(shell cat package.ignore | tr "\\n" "|" | sed "s/.$$//")
 ELECTRON_VERSION=0.36.0
+APPLICATION_NAME="ResinEtcher"
 
 release/ResinEtcher-darwin-x64: .
-	$(ELECTRON_PACKAGER) . "Resin Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=darwin \
 		--arch=x64 \
 		--version=$(ELECTRON_VERSION) \
@@ -14,7 +16,7 @@ release/ResinEtcher-darwin-x64: .
 		--out=release/
 
 release/ResinEtcher-linux-ia32: .
-	$(ELECTRON_PACKAGER) . "Resin Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=linux \
 		--arch=ia32 \
 		--version=$(ELECTRON_VERSION) \
@@ -24,7 +26,7 @@ release/ResinEtcher-linux-ia32: .
 		--out=release/
 
 release/ResinEtcher-linux-x64: .
-	$(ELECTRON_PACKAGER) . "Resin Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=linux \
 		--arch=x64 \
 		--version=$(ELECTRON_VERSION) \
@@ -34,7 +36,7 @@ release/ResinEtcher-linux-x64: .
 		--out=release/
 
 release/ResinEtcher-win32-ia32: .
-	$(ELECTRON_PACKAGER) . "Resin Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=win32 \
 		--arch=ia32 \
 		--version=$(ELECTRON_VERSION) \
@@ -45,7 +47,7 @@ release/ResinEtcher-win32-ia32: .
 		--out=release/
 
 release/ResinEtcher-win32-x64: .
-	$(ELECTRON_PACKAGER) . "Resin Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=win32 \
 		--arch=x64 \
 		--version=$(ELECTRON_VERSION) \
@@ -59,6 +61,14 @@ package-osx: release/ResinEtcher-darwin-x64
 package-linux: release/ResinEtcher-linux-ia32 release/ResinEtcher-linux-x64
 package-win32: release/ResinEtcher-win32-ia32 release/ResinEtcher-win32-x64
 package-all: package-osx package-linux package-win32
+
+installer-osx-x64: release/ResinEtcher-darwin-x64
+	$(ELECTRON_BUILDER) "$</$(APPLICATION_NAME).app" \
+		--platform=osx \
+		--out=release/installers \
+		--config=installer.json
+
+installer-osx: installer-osx-x64
 
 clean:
 	rm -rf release/

balena-io/etcher

Makefile
@@ -1,9 +1,11 @@
 ELECTRON_PACKAGER=./node_modules/.bin/electron-packager
+ELECTRON_BUILDER=./node_modules/.bin/electron-builder
 ELECTRON_IGNORE=$(shell cat package.ignore | tr "\\n" "|" | sed "s/.$$//")
 ELECTRON_VERSION=0.36.0
+APPLICATION_NAME="Etcher"
 
 release/Etcher-darwin-x64: .
-	$(ELECTRON_PACKAGER) . "Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=darwin \
 		--arch=x64 \
 		--version=$(ELECTRON_VERSION) \
@@ -14,7 +16,7 @@ release/Etcher-darwin-x64: .
 		--out=release/
 
 release/Etcher-linux-ia32: .
-	$(ELECTRON_PACKAGER) . "Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=linux \
 		--arch=ia32 \
 		--version=$(ELECTRON_VERSION) \
@@ -24,7 +26,7 @@ release/Etcher-linux-ia32: .
 		--out=release/
 
 release/Etcher-linux-x64: .
-	$(ELECTRON_PACKAGER) . "Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=linux \
 		--arch=x64 \
 		--version=$(ELECTRON_VERSION) \
@@ -34,7 +36,7 @@ release/Etcher-linux-x64: .
 		--out=release/
 
 release/Etcher-win32-ia32: .
-	$(ELECTRON_PACKAGER) . "Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=win32 \
 		--arch=ia32 \
 		--version=$(ELECTRON_VERSION) \
@@ -45,7 +47,7 @@ release/Etcher-win32-ia32: .
 		--out=release/
 
 release/Etcher-win32-x64: .
-	$(ELECTRON_PACKAGER) . "Etcher" \
+	$(ELECTRON_PACKAGER) . $(APPLICATION_NAME) \
 		--platform=win32 \
 		--arch=x64 \
 		--version=$(ELECTRON_VERSION) \
@@ -59,6 +61,13 @@ package-osx: release/Etcher-darwin-x64
 package-linux: release/Etcher-linux-ia32 release/Etcher-linux-x64
 package-win32: release/Etcher-win32-ia32 release/Etcher-win32-x64
 package-all: package-osx package-linux package-win32
+
+release/installers/Etcher.dmg: release/Etcher-darwin-x64 package.json
+	$(ELECTRON_BUILDER) "$</$(APPLICATION_NAME).app" \
+		--platform=osx \
+		--out=release/installers
+
+installer-osx: release/installers/Etcher.dmg
 
 clean:
 	rm -rf release/