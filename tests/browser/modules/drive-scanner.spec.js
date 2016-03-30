34907765dffe0e75bfedef078748a1a19f25de25
Juan Cruz Viotti <jviottidc@gmail.com>
2016-03-30 16:51:39 -0400

    Merge src/drives.js with DriveScannerService
    `src/drives.js` made little on its own, and only caused extra thinking
    overhead due to indirection.

--- tests/browser/modules/drive-scanner.spec.js
+++ tests/browser/modules/drive-scanner.spec.js
@@ -2,6 +2,7 @@
 
 const m = require('mochainon');
 const angular = require('angular');
+const drivelist = require('drivelist');
 require('angular-mocks');
 require('../../../lib/browser/modules/drive-scanner');
 
@@ -12,12 +13,14 @@ describe('Browser: DriveScanner', function() {
   describe('DriveScannerService', function() {
 
     let $interval;
+    let $rootScope;
     let $timeout;
     let $q;
     let DriveScannerService;
 
-    beforeEach(angular.mock.inject(function(_$interval_, _$timeout_, _$q_, _DriveScannerService_) {
+    beforeEach(angular.mock.inject(function(_$interval_, _$rootScope_, _$timeout_, _$q_, _DriveScannerService_) {
       $interval = _$interval_;
+      $rootScope = _$rootScope_;
       $timeout = _$timeout_;
       $q = _$q_;
       DriveScannerService = _DriveScannerService_;
@@ -25,6 +28,148 @@ describe('Browser: DriveScanner', function() {
 
     it('should have no drives by default', function() {
       m.chai.expect(DriveScannerService.drives).to.deep.equal([]);
+    });
+
+    describe('.scan()', function() {
+
+      describe('given no available drives', function() {
+
+        beforeEach(function() {
+          this.drivesListStub = m.sinon.stub(drivelist, 'list');
+          this.drivesListStub.yields(null, []);
+        });
+
+        afterEach(function() {
+          this.drivesListStub.restore();
+        });
+
+        it('should eventually equal an empty array', function(done) {
+          DriveScannerService.scan().then(function(drives) {
+            m.chai.expect(drives).to.deep.equal([]);
+            done();
+          });
+
+          $rootScope.$apply();
+        });
+
+      });
+
+      describe('given available system drives', function() {
+
+        beforeEach(function() {
+          this.drives = [
+            {
+              device: '/dev/sda',
+              description: 'WDC WD10JPVX-75J',
+              size: '931.5G',
+              mountpoint: '/',
+              system: true
+            }
+          ];
+
+          this.drivesListStub = m.sinon.stub(drivelist, 'list');
+          this.drivesListStub.yields(null, this.drives);
+        });
+
+        afterEach(function() {
+          this.drivesListStub.restore();
+        });
+
+        it('should eventually equal an empty array', function(done) {
+          DriveScannerService.scan().then(function(drives) {
+            m.chai.expect(drives).to.deep.equal([]);
+            done();
+          });
+
+          $rootScope.$apply();
+        });
+
+      });
+
+      describe('given available system and removable drives', function() {
+
+        beforeEach(function() {
+          this.drives = [
+            {
+              device: '/dev/sda',
+              description: 'WDC WD10JPVX-75J',
+              size: '931.5G',
+              mountpoint: '/',
+              system: true
+            },
+            {
+              device: '/dev/sdb',
+              description: 'Foo',
+              size: '14G',
+              mountpoint: '/mnt/foo',
+              system: false
+            },
+            {
+              device: '/dev/sdc',
+              description: 'Bar',
+              size: '14G',
+              mountpoint: '/mnt/bar',
+              system: false
+            }
+          ];
+
+          this.drivesListStub = m.sinon.stub(drivelist, 'list');
+          this.drivesListStub.yields(null, this.drives);
+        });
+
+        afterEach(function() {
+          this.drivesListStub.restore();
+        });
+
+        it('should eventually become the removable drives', function(done) {
+          DriveScannerService.scan().then(function(drives) {
+            m.chai.expect(drives).to.deep.equal([
+              {
+                device: '/dev/sdb',
+                description: 'Foo',
+                size: '14G',
+                mountpoint: '/mnt/foo',
+                system: false
+              },
+              {
+                device: '/dev/sdc',
+                description: 'Bar',
+                size: '14G',
+                mountpoint: '/mnt/bar',
+                system: false
+              }
+            ]);
+            done();
+          });
+
+          $rootScope.$apply();
+        });
+
+      });
+
+      describe('given an error when listing the drives', function() {
+
+        beforeEach(function() {
+          this.drivesListStub = m.sinon.stub(drivelist, 'list');
+          this.drivesListStub.yields(new Error('scan error'));
+        });
+
+        afterEach(function() {
+          this.drivesListStub.restore();
+        });
+
+        it('should be rejected with the error', function(done) {
+          DriveScannerService.scan().catch(function(error) {
+            m.chai.expect(error).to.be.an.instanceof(Error);
+            m.chai.expect(error.message).to.equal('scan error');
+            done();
+          });
+
+          $rootScope.$apply();
+        });
+
+      });
+
     });
 
     describe('given no drives', function() {
