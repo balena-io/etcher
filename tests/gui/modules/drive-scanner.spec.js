991b69c17e700479f747b6b582aff661c8c363f6
Juan Cruz Viotti <jviottidc@gmail.com>
2016-06-22 10:41:36 -0400

    refactor(DriveScannerService): use RxJS (#505)
    The concept of a drive scanner lends itself very well to the concept of
    "reactive programming", since the "available drives" make perfect sense
    as an "Observable".
    
    For this reason, the module was re-implemented using RxJS, which greatly
    simplifies things and erradicates certain edge cases we were protecting
    ourselves from automatically.
    
    Other changes made in this PR:
    
    - `DriveScannerService` no longer requires `DrivesModel`. The
    `DriveScannerService` is the one in charge of populating the model.
    
    - `DriveScannerService.scan()` no longer returns an `EventEmitter`. The
    service itself is now an `EventEmitter`.
    
    - `DriveScannerService` now emits a `drives`, not a `scan` event.
    
    Signed-off-by: Juan Cruz Viotti <jviottidc@gmail.com>

--- tests/gui/modules/drive-scanner.spec.js
+++ tests/gui/modules/drive-scanner.spec.js
@@ -11,166 +11,64 @@ describe('Browser: DriveScanner', function() {
     require('../../../lib/gui/modules/drive-scanner')
   ));
 
-  beforeEach(angular.mock.module(
-    require('../../../lib/gui/models/drives')
-  ));
-
   describe('DriveScannerService', function() {
 
-    let $interval;
-    let $rootScope;
-    let $timeout;
-    let $q;
-    let DrivesModel;
     let DriveScannerService;
 
-    beforeEach(angular.mock.inject(function(_$interval_, _$rootScope_, _$timeout_, _$q_, _DriveScannerService_, _DrivesModel_) {
-      $interval = _$interval_;
-      $rootScope = _$rootScope_;
-      $timeout = _$timeout_;
-      $q = _$q_;
+    beforeEach(angular.mock.inject(function(_DriveScannerService_) {
       DriveScannerService = _DriveScannerService_;
-      DrivesModel = _DrivesModel_;
     }));
 
-    describe('.scan()', function() {
+    describe('given no available drives', function() {
 
-      describe('given no available drives', function() {
-
-        beforeEach(function() {
-          this.drivesListStub = m.sinon.stub(drivelist, 'list');
-          this.drivesListStub.yields(null, []);
-        });
-
-        afterEach(function() {
-          this.drivesListStub.restore();
-        });
-
-        it('should eventually equal an empty array', function(done) {
-          DriveScannerService.scan().then(function(drives) {
-            m.chai.expect(drives).to.deep.equal([]);
-            done();
-          });
-
-          $rootScope.$apply();
-        });
-
+      beforeEach(function() {
+        this.drivesListStub = m.sinon.stub(drivelist, 'list');
+        this.drivesListStub.yields(null, []);
       });
 
-      describe('given available system drives', function() {
-
-        beforeEach(function() {
-          this.drives = [
-            {
-              device: '/dev/sda',
-              description: 'WDC WD10JPVX-75J',
-              size: '931.5G',
-              mountpoint: '/',
-              system: true
-            }
-          ];
-
-          this.drivesListStub = m.sinon.stub(drivelist, 'list');
-          this.drivesListStub.yields(null, this.drives);
-        });
-
-        afterEach(function() {
-          this.drivesListStub.restore();
-        });
-
-        it('should eventually equal an empty array', function(done) {
-          DriveScannerService.scan().then(function(drives) {
-            m.chai.expect(drives).to.deep.equal([]);
-            done();
-          });
-
-          $rootScope.$apply();
-        });
-
+      afterEach(function() {
+        this.drivesListStub.restore();
       });
 
-      describe('given available system and removable drives', function() {
-
-        beforeEach(function() {
-          this.drives = [
-            {
-              device: '/dev/sda',
-              description: 'WDC WD10JPVX-75J',
-              size: '931.5G',
-              mountpoint: '/',
-              system: true
-            },
-            {
-              device: '/dev/sdb',
-              description: 'Foo',
-              size: '14G',
-              mountpoint: '/mnt/foo',
-              system: false
-            },
-            {
-              device: '/dev/sdc',
-              description: 'Bar',
-              size: '14G',
-              mountpoint: '/mnt/bar',
-              system: false
-            }
-          ];
-
-          this.drivesListStub = m.sinon.stub(drivelist, 'list');
-          this.drivesListStub.yields(null, this.drives);
+      it('should emit an empty array', function(done) {
+        DriveScannerService.on('drives', function(drives) {
+          m.chai.expect(drives).to.deep.equal([]);
+          DriveScannerService.stop();
+          done();
         });
 
-        afterEach(function() {
-          this.drivesListStub.restore();
-        });
-
-        it('should eventually become the removable drives', function(done) {
-          DriveScannerService.scan().then(function(drives) {
-            m.chai.expect(drives).to.deep.equal([
-              {
-                device: '/dev/sdb',
-                description: 'Foo',
-                size: '14G',
-                mountpoint: '/mnt/foo',
-                system: false
-              },
-              {
-                device: '/dev/sdc',
-                description: 'Bar',
-                size: '14G',
-                mountpoint: '/mnt/bar',
-                system: false
-              }
-            ]);
-            done();
-          });
-
-          $rootScope.$apply();
-        });
-
+        DriveScannerService.start();
       });
 
-      describe('given an error when listing the drives', function() {
+    });
 
-        beforeEach(function() {
-          this.drivesListStub = m.sinon.stub(drivelist, 'list');
-          this.drivesListStub.yields(new Error('scan error'));
+    describe('given only system available drives', function() {
+
+      beforeEach(function() {
+        this.drivesListStub = m.sinon.stub(drivelist, 'list');
+        this.drivesListStub.yields(null, [
+          {
+            device: '/dev/sda',
+            description: 'WDC WD10JPVX-75J',
+            size: '931.5G',
+            mountpoint: '/',
+            system: true
+          }
+        ]);
+      });
+
+      afterEach(function() {
+        this.drivesListStub.restore();
+      });
+
+      it('should emit an empty array', function(done) {
+        DriveScannerService.on('drives', function(drives) {
+          m.chai.expect(drives).to.deep.equal([]);
+          DriveScannerService.stop();
+          done();
         });
 
-        afterEach(function() {
-          this.drivesListStub.restore();
-        });
-
-        it('should be rejected with the error', function(done) {
-          DriveScannerService.scan().catch(function(error) {
-            m.chai.expect(error).to.be.an.instanceof(Error);
-            m.chai.expect(error.message).to.equal('scan error');
-            done();
-          });
-
-          $rootScope.$apply();
-        });
-
+        DriveScannerService.start();
       });
 
     });
@@ -178,7 +76,15 @@ describe('Browser: DriveScanner', function() {
     describe('given available drives', function() {
 
       beforeEach(function() {
-        this.drives = [
+        this.drivesListStub = m.sinon.stub(drivelist, 'list');
+        this.drivesListStub.yields(null, [
+          {
+            device: '/dev/sda',
+            description: 'WDC WD10JPVX-75J',
+            size: '931.5G',
+            mountpoint: '/',
+            system: true
+          },
           {
             device: '/dev/sdb',
             description: 'Foo',
@@ -193,37 +99,61 @@ describe('Browser: DriveScanner', function() {
             mountpoint: '/mnt/bar',
             system: false
           }
-        ];
-
-        this.scanStub = m.sinon.stub(DriveScannerService, 'scan');
-        this.scanStub.returns($q.resolve(this.drives));
+        ]);
       });
 
       afterEach(function() {
-        this.scanStub.restore();
+        this.drivesListStub.restore();
       });
 
-      it('should set the drives to the scanned ones', function() {
-        DriveScannerService.start(200);
-        $timeout.flush();
-        $interval.flush(400);
-        m.chai.expect(DrivesModel.getDrives()).to.deep.equal(this.drives);
-        DriveScannerService.stop();
-      });
+      it('should emit the non removable drives', function(done) {
+        DriveScannerService.on('drives', function(drives) {
+          m.chai.expect(drives).to.deep.equal([
+            {
+              device: '/dev/sdb',
+              description: 'Foo',
+              size: '14G',
+              mountpoint: '/mnt/foo',
+              system: false
+            },
+            {
+              device: '/dev/sdc',
+              description: 'Bar',
+              size: '14G',
+              mountpoint: '/mnt/bar',
+              system: false
+            }
+          ]);
 
-      describe('.start()', function() {
-
-        it('should emit a `scan` event with the drives', function() {
-          const emitter = DriveScannerService.start(2000);
-          const scanSpy = m.sinon.spy();
-          emitter.on('scan', scanSpy);
-          $timeout.flush();
-          $interval.flush(1000);
-          m.chai.expect(scanSpy).to.have.been.calledOnce;
-          m.chai.expect(scanSpy).to.have.been.calledWith(this.drives);
           DriveScannerService.stop();
+          done();
         });
 
+        DriveScannerService.start();
+      });
+
+    });
+
+    describe('given an error when listing the drives', function() {
+
+      beforeEach(function() {
+        this.drivesListStub = m.sinon.stub(drivelist, 'list');
+        this.drivesListStub.yields(new Error('scan error'));
+      });
+
+      afterEach(function() {
+        this.drivesListStub.restore();
+      });
+
+      it('should emit the error', function(done) {
+        DriveScannerService.on('error', function(error) {
+          m.chai.expect(error).to.be.an.instanceof(Error);
+          m.chai.expect(error.message).to.equal('scan error');
+          DriveScannerService.stop();
+          done();
+        });
+
+        DriveScannerService.start();
       });
 
     });
