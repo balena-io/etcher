9658d371ecde0f5a062a1e6f2680e2a0bd1dbc80
Juan Cruz Viotti <jviottidc@gmail.com>
2016-06-29 13:05:21 -0400

    refactor(GUI): make use of `etcher-latest-version`
    The code that performs an HTTP request to the S3 bucket where released
    are stored and determines which is the latest available version was
    extracted to a separate module called `etcher-latest-version`, mainly
    for the website to be able to re-use this functionality.
    
    See: https://github.com/resin-io-modules/etcher-latest-version
    Signed-off-by: Juan Cruz Viotti <jviottidc@gmail.com>

--- tests/gui/components/update-notifier.spec.js
+++ tests/gui/components/update-notifier.spec.js
@@ -102,22 +102,21 @@ describe('Browser: UpdateNotifier', function() {
         let UpdateNotifierService;
         let ManifestBindService;
 
-        beforeEach(function() {
-          angular.mock.module(function($provide) {
-            $provide.value('UpdateNotifierS3Service', {
-              getLatestVersion: function() {
-                return $q.resolve(ManifestBindService.get('version'));
-              }
-            });
-          });
-        });
-
         beforeEach(angular.mock.inject(function(_$q_, _$rootScope_, _UpdateNotifierService_, _ManifestBindService_) {
           $q = _$q_;
           $rootScope = _$rootScope_;
           UpdateNotifierService = _UpdateNotifierService_;
           ManifestBindService = _ManifestBindService_;
         }));
+
+        beforeEach(function() {
+          this.getLatestVersionStub = m.sinon.stub(UpdateNotifierService, 'getLatestVersion');
+          this.getLatestVersionStub.returns($q.resolve(ManifestBindService.get('version')));
+        });
+
+        afterEach(function() {
+          this.getLatestVersionStub.restore();
+        });
 
         it('should resolve true', function() {
           let result = null;
@@ -138,21 +137,20 @@ describe('Browser: UpdateNotifier', function() {
         let $rootScope;
         let UpdateNotifierService;
 
-        beforeEach(function() {
-          angular.mock.module(function($provide) {
-            $provide.value('UpdateNotifierS3Service', {
-              getLatestVersion: function() {
-                return $q.resolve('99999.9.9');
-              }
-            });
-          });
-        });
-
         beforeEach(angular.mock.inject(function(_$q_, _$rootScope_, _UpdateNotifierService_) {
           $q = _$q_;
           $rootScope = _$rootScope_;
           UpdateNotifierService = _UpdateNotifierService_;
         }));
+
+        beforeEach(function() {
+          this.getLatestVersionStub = m.sinon.stub(UpdateNotifierService, 'getLatestVersion');
+          this.getLatestVersionStub.returns($q.resolve('99999.9.9'));
+        });
+
+        afterEach(function() {
+          this.getLatestVersionStub.restore();
+        });
 
         it('should resolve false', function() {
           let result = null;
@@ -173,21 +171,20 @@ describe('Browser: UpdateNotifier', function() {
         let $rootScope;
         let UpdateNotifierService;
 
-        beforeEach(function() {
-          angular.mock.module(function($provide) {
-            $provide.value('UpdateNotifierS3Service', {
-              getLatestVersion: function() {
-                return $q.resolve('0.0.0');
-              }
-            });
-          });
-        });
-
         beforeEach(angular.mock.inject(function(_$q_, _$rootScope_, _UpdateNotifierService_) {
           $q = _$q_;
           $rootScope = _$rootScope_;
           UpdateNotifierService = _UpdateNotifierService_;
         }));
+
+        beforeEach(function() {
+          this.getLatestVersionStub = m.sinon.stub(UpdateNotifierService, 'getLatestVersion');
+          this.getLatestVersionStub.returns($q.resolve('0.0.0'));
+        });
+
+        afterEach(function() {
+          this.getLatestVersionStub.restore();
+        });
 
         it('should resolve true', function() {
           let result = null;
@@ -200,122 +197,6 @@ describe('Browser: UpdateNotifier', function() {
           m.chai.expect(result).to.be.true;
         });
 
-      });
-
-    });
-
-  });
-
-  describe('UpdateNotifierS3Service', function() {
-
-    let UpdateNotifierS3Service;
-    let $rootScope;
-
-    beforeEach(angular.mock.inject(function(_$rootScope_, _UpdateNotifierS3Service_) {
-      $rootScope = _$rootScope_;
-      UpdateNotifierS3Service = _UpdateNotifierS3Service_;
-    }));
-
-    describe('given a mocked S3 XML response', function() {
-
-      let $httpBackend;
-      let UPDATE_NOTIFIER_URL;
-
-      beforeEach(angular.mock.inject(function($injector) {
-        $httpBackend = $injector.get('$httpBackend');
-        UPDATE_NOTIFIER_URL = $injector.get('UPDATE_NOTIFIER_URL');
-
-        $httpBackend.whenGET(UPDATE_NOTIFIER_URL).respond(`
-          <ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-            <Name>resin-production-downloads</Name>
-            <Prefix/>
-            <Marker/>
-            <MaxKeys>1000</MaxKeys>
-            <IsTruncated>false</IsTruncated>
-            <Contents>
-              <Key>etcher/1.0.0-beta.0/Etcher-darwin-x64.dmg</Key>
-              <LastModified>2016-03-10T17:34:21.000Z</LastModified>
-              <ETag>"5a715255aa25686688bf1e23bc1d3fc6"</ETag>
-              <Size>46109720</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.1/Etcher-darwin-x64.dmg</Key>
-              <LastModified>2016-04-08T20:12:03.000Z</LastModified>
-              <ETag>"cc1d6d9d53385e3edd099416fcd894c1"</ETag>
-              <Size>47071474</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.2/Etcher-darwin-x64.dmg</Key>
-              <LastModified>2016-04-08T19:03:18.000Z</LastModified>
-              <ETag>"5f1849f7781197ce2ee6129c16bcd498"</ETag>
-              <Size>48650090</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.3/Etcher-darwin-x64.dmg</Key>
-              <LastModified>2016-04-18T01:32:09.000Z</LastModified>
-              <ETag>"c173895886f44d115c66e7206ce3dff8"</ETag>
-              <Size>50585335</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.3/Etcher-darwin-x64.zip</Key>
-              <LastModified>2016-04-18T01:42:37.000Z</LastModified>
-              <ETag>"e9f6e957e65373b232530215d98df141"</ETag>
-              <Size>129327442</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.4/Etcher-darwin-x64.dmg</Key>
-              <LastModified>2016-04-22T17:29:49.000Z</LastModified>
-              <ETag>"bccb0024c58747a9b7516cbdfc5a7ecb"</ETag>
-              <Size>55240852</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.4/Etcher-darwin-x64.zip</Key>
-              <LastModified>2016-04-22T17:43:27.000Z</LastModified>
-              <ETag>"c93e26e68b3c4f2b7e8e88e6befc8e64"</ETag>
-              <Size>135443284</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.5/Etcher-darwin-x64.dmg</Key>
-              <LastModified>2016-05-04T08:27:11.000Z</LastModified>
-              <ETag>"fb596bfdb8bbaf09807b5fc4a940ce14"</ETag>
-              <Size>77757305</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-            <Contents>
-              <Key>etcher/1.0.0-beta.5/Etcher-darwin-x64.zip</Key>
-              <LastModified>2016-05-04T08:39:56.000Z</LastModified>
-              <ETag>"3f11c1b6f06644f9ceb2aea4b1947fdf"</ETag>
-              <Size>157933876</Size>
-              <StorageClass>STANDARD</StorageClass>
-            </Contents>
-          </ListBucketResult>
-        `);
-      }));
-
-      afterEach(function() {
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
-      });
-
-      it('should resolve the latest version', function() {
-        $httpBackend.expectGET(UPDATE_NOTIFIER_URL);
-
-        let latestVersion = null;
-        UpdateNotifierS3Service.getLatestVersion().then(function(result) {
-          latestVersion = result;
-        });
-
-        $rootScope.$apply();
-        $httpBackend.flush();
-
-        m.chai.expect(latestVersion).to.equal('1.0.0-beta.5');
       });
 
     });
