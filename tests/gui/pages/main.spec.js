72c6163173e5a3d07dc464963fae2afc29cd2e0e
Juan Cruz Viotti <jviottidc@gmail.com>
2016-08-04 11:02:38 -0400

    refactor(GUI): main controller
    The main page controller contained a lot of undocumented and untested
    logic. As a first step towards cleaning up the whole thing, this PR
    introduces the following changes:
    
    - Implement `ImageSelectionController`, `DriveSelectionController`, and
      `FlashController` as children of `MainController`. Each of them is
      used by the appropriate main page "steps", and contains logic specific
      to them. The `MainController` hosts functionality that applies to the
      page as a whole.
    
    - Add JSDoc annotations fo every controller function/property.
    
    - Unit test several controller functions.
    
    - Simplify template logic.
    
    The "GUI fifty-thousand foot view" section in ARCHITECTURE.md has been
    removed since there is no longer a single place where you can see all
    the interactions between components.
    
    Signed-off-by: Juan Cruz Viotti <jviottidc@gmail.com>

---
+++ tests/gui/pages/main.spec.js
@@ -0,0 +1,378 @@
+'use strict';
+
+const m = require('mochainon');
+const angular = require('angular');
+require('angular-mocks');
+
+describe('Browser: MainPage', function() {
+
+  beforeEach(angular.mock.module(
+    require('../../../lib/gui/pages/main/main')
+  ));
+
+  describe('MainController', function() {
+
+    let $controller;
+    let SelectionStateModel;
+    let DrivesModel;
+
+    beforeEach(angular.mock.inject(function(_$controller_, _SelectionStateModel_, _DrivesModel_) {
+      $controller = _$controller_;
+      SelectionStateModel = _SelectionStateModel_;
+      DrivesModel = _DrivesModel_;
+    }));
+
+    describe('.shouldDriveStepBeDisabled()', function() {
+
+      it('should return true if there is no image', function() {
+        const controller = $controller('MainController', {
+          $scope: {}
+        });
+
+        SelectionStateModel.clear();
+
+        m.chai.expect(controller.shouldDriveStepBeDisabled()).to.be.true;
+      });
+
+      it('should return false if there is an image', function() {
+        const controller = $controller('MainController', {
+          $scope: {}
+        });
+
+        SelectionStateModel.setImage({
+          path: 'rpi.img',
+          size: 99999
+        });
+
+        m.chai.expect(controller.shouldDriveStepBeDisabled()).to.be.false;
+      });
+
+    });
+
+    describe('.shouldFlashStateBeDisabled()', function() {
+
+      it('should return true if there is no selected drive nor image', function() {
+        const controller = $controller('MainController', {
+          $scope: {}
+        });
+
+        SelectionStateModel.clear();
+
+        m.chai.expect(controller.shouldFlashStateBeDisabled()).to.be.true;
+      });
+
+      it('should return true if there is a selected image but no drive', function() {
+        const controller = $controller('MainController', {
+          $scope: {}
+        });
+
+        SelectionStateModel.clear();
+        SelectionStateModel.setImage({
+          path: 'rpi.img',
+          size: 99999
+        });
+
+        m.chai.expect(controller.shouldFlashStateBeDisabled()).to.be.true;
+      });
+
+      it('should return true if there is a selected drive but no image', function() {
+        const controller = $controller('MainController', {
+          $scope: {}
+        });
+
+        DrivesModel.setDrives([
+          {
+            device: '/dev/disk2',
+            description: 'Foo',
+            size: 99999,
+            mountpoint: '/mnt/foo',
+            system: false
+          }
+        ]);
+
+        SelectionStateModel.clear();
+        SelectionStateModel.setDrive('/dev/disk2');
+
+        m.chai.expect(controller.shouldFlashStateBeDisabled()).to.be.true;
+      });
+
+      it('should return false if there is a selected drive and a selected image', function() {
+        const controller = $controller('MainController', {
+          $scope: {}
+        });
+
+        DrivesModel.setDrives([
+          {
+            device: '/dev/disk2',
+            description: 'Foo',
+            size: 99999,
+            mountpoint: '/mnt/foo',
+            system: false
+          }
+        ]);
+
+        SelectionStateModel.clear();
+        SelectionStateModel.setDrive('/dev/disk2');
+
+        SelectionStateModel.setImage({
+          path: 'rpi.img',
+          size: 99999
+        });
+
+        m.chai.expect(controller.shouldFlashStateBeDisabled()).to.be.false;
+      });
+
+    });
+
+  });
+
+  describe('ImageSelectionController', function() {
+
+    let $controller;
+    let SupportedFormatsModel;
+
+    beforeEach(angular.mock.inject(function(_$controller_, _SupportedFormatsModel_) {
+      $controller = _$controller_;
+      SupportedFormatsModel = _SupportedFormatsModel_;
+    }));
+
+    it('should contain all available extensions in mainSupportedExtensions and extraSupportedExtensions', function() {
+      const $scope = {};
+      const controller = $controller('ImageSelectionController', {
+        $scope
+      });
+
+      const extensions = controller.mainSupportedExtensions.concat(controller.extraSupportedExtensions);
+      m.chai.expect(extensions).to.deep.equal(SupportedFormatsModel.getAllExtensions());
+    });
+
+  });
+
+  describe('FlashController', function() {
+
+    let $controller;
+    let FlashStateModel;
+    let SettingsModel;
+
+    beforeEach(angular.mock.inject(function(_$controller_, _FlashStateModel_, _SettingsModel_) {
+      $controller = _$controller_;
+      FlashStateModel = _FlashStateModel_;
+      SettingsModel = _SettingsModel_;
+    }));
+
+    describe('.getProgressButtonLabel()', function() {
+
+      it('should return "Flash!" given a clean state', function() {
+        const controller = $controller('FlashController', {
+          $scope: {}
+        });
+
+        FlashStateModel.resetState();
+        m.chai.expect(controller.getProgressButtonLabel()).to.equal('Flash!');
+      });
+
+      describe('given there is a flash in progress', function() {
+
+        beforeEach(function() {
+          FlashStateModel.setFlashingFlag();
+        });
+
+        it('should handle percentage == 0, type = write, unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'write',
+            percentage: 0,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', true);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Starting...');
+        });
+
+        it('should handle percentage == 0, type = write, !unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'write',
+            percentage: 0,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', false);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Starting...');
+        });
+
+        it('should handle percentage == 0, type = check, unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'check',
+            percentage: 0,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', true);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Starting...');
+        });
+
+        it('should handle percentage == 0, type = check, !unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'check',
+            percentage: 0,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', false);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Starting...');
+        });
+
+        it('should handle percentage == 50, type = write, unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'write',
+            percentage: 50,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', true);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('50%');
+        });
+
+        it('should handle percentage == 50, type = write, !unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'write',
+            percentage: 50,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', false);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('50%');
+        });
+
+        it('should handle percentage == 50, type = check, unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'check',
+            percentage: 50,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', true);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('50% Validating...');
+        });
+
+        it('should handle percentage == 50, type = check, !unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'check',
+            percentage: 50,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', false);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('50% Validating...');
+        });
+
+        it('should handle percentage == 100, type = write, unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'write',
+            percentage: 100,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', true);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Finishing...');
+        });
+
+        it('should handle percentage == 100, type = write, !unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'write',
+            percentage: 100,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', false);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Finishing...');
+        });
+
+        it('should handle percentage == 100, type = check, unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'check',
+            percentage: 100,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', true);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Unmounting...');
+        });
+
+        it('should handle percentage == 100, type = check, !unmountOnSuccess', function() {
+          const controller = $controller('FlashController', {
+            $scope: {}
+          });
+
+          FlashStateModel.setProgressState({
+            type: 'check',
+            percentage: 100,
+            eta: 15,
+            speed: 1000
+          });
+
+          SettingsModel.set('unmountOnSuccess', false);
+          m.chai.expect(controller.getProgressButtonLabel()).to.equal('Finishing...');
+        });
+
+      });
+
+    });
+
+  });
+
+});
