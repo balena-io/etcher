3fbfcdb953b86a6a76413da18997ce83380a485a
Juan Cruz Viotti <jviotti@openmailbox.org>
2017-02-02 15:08:37 -0400

    chore: add support for Windows builds in Makefile
    This commits introduces the following Windows rules:
    
    - `electron-installer-zip`
    - `electron-installer-nsis`
    
    Other changes:
    
    - Remove `scripts/build/windows.bat`
    - Update `docs/RUNNING-LOCALLY.md` to point to Makefile
    - Update `docs/PUBLISHING.md` to point to Makefile
    - Remove `7z` from `docs/RUNNING-LOCALLY.md` pre-requisites
    
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

---
+++ scripts/build/electron-installer-zip-win32.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+###
+# Copyright 2016 resin.io
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+set -u
+set -e
+
+./scripts/build/check-dependency.sh zip
+
+function usage() {
+  echo "Usage: $0"
+  echo ""
+  echo "Options"
+  echo ""
+  echo "    -a <application package directory>"
+  echo "    -o <output>"
+  exit 1
+}
+
+ARGV_APPLICATION=""
+ARGV_OUTPUT=""
+
+while getopts ":a:o:" option; do
+  case $option in
+    a) ARGV_APPLICATION="$OPTARG" ;;
+    o) ARGV_OUTPUT="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
+if [ -z "$ARGV_APPLICATION" ] || [ -z "$ARGV_OUTPUT" ]; then
+  usage
+fi
+
+CWD=$(pwd)
+pushd "$ARGV_APPLICATION"
+zip -r -9 "$CWD/$ARGV_OUTPUT" *
+popd
