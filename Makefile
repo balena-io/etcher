pine64dev/PINE64-Installer

chore: automatic CLI packaging
This commit introduces the phony `package-cli` target, used to package
the Electron CLI in a directory.

Other related changes:

- The `package` target has been renamed to `package-electron`

Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

Makefile
@@ -19,6 +19,7 @@ BUILD_OUTPUT_DIRECTORY = $(BUILD_DIRECTORY)/out
 # ---------------------------------------------------------------------
 
 ELECTRON_VERSION = $(shell jq -r '.devDependencies["electron-prebuilt"]' package.json)
+NODE_VERSION = 6.1.0
 COMPANY_NAME = $(shell jq -r '.companyName' package.json)
 APPLICATION_NAME = $(shell jq -r '.displayName' package.json)
 APPLICATION_DESCRIPTION = $(shell jq -r '.description' package.json)
@@ -170,6 +171,9 @@ $(BUILD_TEMPORARY_DIRECTORY): | $(BUILD_DIRECTORY)
 $(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies: | $(BUILD_DIRECTORY)
 	mkdir $@
 
+$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies: | $(BUILD_DIRECTORY)
+	mkdir $@
+
 $(BUILD_OUTPUT_DIRECTORY): | $(BUILD_DIRECTORY)
 	mkdir $@
 
@@ -180,6 +184,15 @@ $(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_
 		-v "$(ELECTRON_VERSION)" \
 		-x $| \
 		-t electron \
+		-s "$(TARGET_PLATFORM)"
+
+$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_modules: package.json npm-shrinkwrap.json \
+	| $(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies
+	./scripts/build/dependencies-npm.sh -p -f \
+		-r "$(TARGET_ARCH)" \
+		-v "$(NODE_VERSION)" \
+		-x $| \
+		-t node \
 		-s "$(TARGET_PLATFORM)"
 
 $(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/bower_components: bower.json \
@@ -207,6 +220,28 @@ $(BUILD_DIRECTORY)/electron-$(ELECTRON_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH
 		-v "$(ELECTRON_VERSION)" \
 		-s "$(TARGET_PLATFORM)" \
 		-o $@
+
+$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH)-app: \
+	package.json lib \
+	$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_modules \
+	| $(BUILD_DIRECTORY)
+	mkdir $@
+	$(foreach prerequisite,$^,$(call execute-command,cp -rf $(prerequisite) $@))
+
+$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH).js: \
+	$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH)-app \
+	| $(BUILD_DIRECTORY)
+	./scripts/build/concatenate-javascript.sh -e $</lib/cli/etcher.js -o $@
+
+$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH): \
+	$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_modules \
+	$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH).js \
+	| $(BUILD_DIRECTORY)
+	./scripts/build/node-package-cli.sh -o $@ -m $< \
+		-n $(APPLICATION_NAME) \
+		-e $(word 2,$^) \
+		-r $(TARGET_ARCH) \
+		-s $(TARGET_PLATFORM)
 
 $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH): \
 	$(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH)-app.asar \
@@ -332,10 +367,13 @@ TARGETS = \
 	info \
 	clean \
 	distclean \
-	package \
+	package-electron \
+	package-cli \
+	cli-develop \
 	electron-develop
 
-package: $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH)
+package-electron: $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH)
+package-cli: $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH)
 
 ifeq ($(TARGET_PLATFORM),darwin)
 electron-installer-app-zip: $(BUILD_OUTPUT_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH).zip
@@ -396,7 +434,7 @@ endif
 
 .PHONY: $(TARGETS)
 
-electron-develop:
+# Note that we need to remove `node_modules` every time.
 # Since we use an `npm-shrinkwrap.json` file, if you pull changes
 # that update a dependency and try to `npm install` directly, npm
 # will complain that your `node_modules` tree is not equal to what
@@ -405,6 +443,16 @@ electron-develop:
 # The `node_modules` directory also needs to be wiped out if you're
 # changing between target architectures, since compiled add-ons
 # will not work otherwise.
+
+cli-develop:
+	rm -rf node_modules
+	./scripts/build/dependencies-npm.sh \
+		-r "$(TARGET_ARCH)" \
+		-v "$(ELECTRON_VERSION)" \
+		-t node \
+		-s "$(TARGET_PLATFORM)"
+
+electron-develop:
 	rm -rf node_modules
 	./scripts/build/dependencies-npm.sh \
 		-r "$(TARGET_ARCH)" \

scripts/build/node-package-cli.sh
@@ -1,0 +1,85 @@
+#!/bin/bash
+
+###
+# Copyright 2016 resin.io
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+set -u
+set -e
+
+./scripts/build/check-dependency.sh upx
+
+function usage() {
+  echo "Usage: $0"
+  echo ""
+  echo "Options"
+  echo ""
+  echo "    -n <application name>"
+  echo "    -e <application entry point (.js)>"
+  echo "    -m <node_modules directory>"
+  echo "    -r <architecture>"
+  echo "    -s <operating system (linux|darwin|win32)>"
+  echo "    -o <output directory>"
+  exit 1
+}
+
+ARGV_APPLICATION_NAME=""
+ARGV_ENTRY_POINT=""
+ARGV_NODE_MODULES=""
+ARGV_ARCHITECTURE=""
+ARGV_OPERATING_SYSTEM=""
+ARGV_OUTPUT=""
+
+while getopts ":n:e:m:r:s:o:" option; do
+  case $option in
+    n) ARGV_APPLICATION_NAME="$OPTARG" ;;
+    e) ARGV_ENTRY_POINT="$OPTARG" ;;
+    m) ARGV_NODE_MODULES="$OPTARG" ;;
+    r) ARGV_ARCHITECTURE="$OPTARG" ;;
+    s) ARGV_OPERATING_SYSTEM="$OPTARG" ;;
+    o) ARGV_OUTPUT="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
+if [ -z "$ARGV_APPLICATION_NAME" ] \
+  || [ -z "$ARGV_ENTRY_POINT" ] \
+  || [ -z "$ARGV_NODE_MODULES" ] \
+  || [ -z "$ARGV_ARCHITECTURE" ] \
+  || [ -z "$ARGV_OPERATING_SYSTEM" ] \
+  || [ -z "$ARGV_OUTPUT" ]; then
+  usage
+fi
+
+mkdir "$ARGV_OUTPUT"
+cp "$ARGV_ENTRY_POINT" "$ARGV_OUTPUT/index.js"
+
+./scripts/build/dependencies-npm-extract-addons.sh \
+  -d "$ARGV_NODE_MODULES" \
+  -o "$ARGV_OUTPUT/node_modules"
+
+APPLICATION_NAME_LOWERCASE="$(echo "$ARGV_APPLICATION_NAME" | tr '[:upper:]' '[:lower:]')"
+BINARY_LOCATION="$ARGV_OUTPUT/$APPLICATION_NAME_LOWERCASE"
+if [ "$ARGV_OPERATING_SYSTEM" == "win32" ]; then
+  BINARY_LOCATION="$BINARY_LOCATION.exe"
+fi
+
+./scripts/build/node-static-entry-point-download.sh \
+  -r "$ARGV_ARCHITECTURE" \
+  -v "1.0.0" \
+  -s "$ARGV_OPERATING_SYSTEM" \
+  -o "$BINARY_LOCATION"
+chmod +x "$BINARY_LOCATION"
+upx -9 "$BINARY_LOCATION"

balena-io/etcher

Makefile
@@ -19,6 +19,7 @@ BUILD_OUTPUT_DIRECTORY = $(BUILD_DIRECTORY)/out
 # ---------------------------------------------------------------------
 
 ELECTRON_VERSION = $(shell jq -r '.devDependencies["electron-prebuilt"]' package.json)
+NODE_VERSION = 6.1.0
 COMPANY_NAME = $(shell jq -r '.companyName' package.json)
 APPLICATION_NAME = $(shell jq -r '.displayName' package.json)
 APPLICATION_DESCRIPTION = $(shell jq -r '.description' package.json)
@@ -169,6 +170,9 @@ $(BUILD_TEMPORARY_DIRECTORY): | $(BUILD_DIRECTORY)
 $(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies: | $(BUILD_DIRECTORY)
 	mkdir $@
 
+$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies: | $(BUILD_DIRECTORY)
+	mkdir $@
+
 $(BUILD_OUTPUT_DIRECTORY): | $(BUILD_DIRECTORY)
 	mkdir $@
 
@@ -179,6 +183,15 @@ $(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_
 		-v "$(ELECTRON_VERSION)" \
 		-x $| \
 		-t electron \
+		-s "$(TARGET_PLATFORM)"
+
+$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_modules: package.json npm-shrinkwrap.json \
+	| $(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies
+	./scripts/build/dependencies-npm.sh -p -f \
+		-r "$(TARGET_ARCH)" \
+		-v "$(NODE_VERSION)" \
+		-x $| \
+		-t node \
 		-s "$(TARGET_PLATFORM)"
 
 $(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/bower_components: bower.json \
@@ -207,6 +220,57 @@ $(BUILD_DIRECTORY)/electron-$(ELECTRON_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH
 		-s "$(TARGET_PLATFORM)" \
 		-o $@
 
+$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH)-app: \
+	package.json lib \
+	$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_modules \
+	| $(BUILD_DIRECTORY)
+	mkdir $@
+	$(foreach prerequisite,$^,$(call execute-command,cp -rf $(prerequisite) $@))
+
+$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH).js: \
+	$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH)-app \
+	| $(BUILD_DIRECTORY)
+	./scripts/build/concatenate-javascript.sh -e $</lib/cli/etcher.js -o $@
+
+$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH): \
+	$(BUILD_DIRECTORY)/node-$(TARGET_PLATFORM)-$(TARGET_ARCH)-dependencies/node_modules \
+	$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH).js \
+	| $(BUILD_DIRECTORY) $(BUILD_TEMPORARY_DIRECTORY)
+	./scripts/build/node-package-cli.sh -o $@ -l $< \
+		-n $(APPLICATION_NAME) \
+		-e $(word 2,$^) \
+		-r $(TARGET_ARCH) \
+		-s $(TARGET_PLATFORM)
+
+ifeq ($(TARGET_PLATFORM),win32)
+	./scripts/build/electron-brand-exe.sh \
+		-f $@/etcher.exe \
+		-n $(APPLICATION_NAME) \
+		-d "$(APPLICATION_DESCRIPTION)" \
+		-v "$(APPLICATION_VERSION)" \
+		-c "$(APPLICATION_COPYRIGHT)" \
+		-m "$(COMPANY_NAME)" \
+		-i assets/icon.ico \
+		-w $(BUILD_TEMPORARY_DIRECTORY)
+endif
+
+ifeq ($(TARGET_PLATFORM),darwin)
+ifdef CODE_SIGN_IDENTITY
+	./scripts/build/electron-sign-file-darwin.sh -f $@/etcher -i "$(CODE_SIGN_IDENTITY)"
+endif
+endif
+
+ifeq ($(TARGET_PLATFORM),win32)
+ifdef CODE_SIGN_CERTIFICATE
+ifdef CODE_SIGN_CERTIFICATE_PASSWORD
+	./scripts/build/electron-sign-exe-win32.sh -f $@/etcher.exe \
+		-d "$(APPLICATION_NAME) - $(APPLICATION_VERSION)" \
+		-c $(CODE_SIGN_CERTIFICATE) \
+		-p $(CODE_SIGN_CERTIFICATE_PASSWORD)
+endif
+endif
+endif
+
 $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH): \
 	$(BUILD_DIRECTORY)/electron-$(TARGET_PLATFORM)-$(APPLICATION_VERSION)-$(TARGET_ARCH)-app.asar \
 	$(BUILD_DIRECTORY)/electron-$(ELECTRON_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH).zip \
@@ -221,6 +285,7 @@ ifeq ($(TARGET_PLATFORM),darwin)
 		-i assets/icon.icns \
 		-o $@
 endif
+
 ifeq ($(TARGET_PLATFORM),linux)
 	./scripts/build/electron-configure-package-linux.sh -p $(word 2,$^) -a $< \
 		-n "$(APPLICATION_NAME)" \
@@ -228,6 +293,7 @@ ifeq ($(TARGET_PLATFORM),linux)
 		-l LICENSE \
 		-o $@
 endif
+
 ifeq ($(TARGET_PLATFORM),win32)
 	./scripts/build/electron-configure-package-win32.sh -p $(word 2,$^) -a $< \
 		-n "$(APPLICATION_NAME)" \
@@ -313,6 +379,7 @@ $(BUILD_OUTPUT_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-win32-$(TAR
 	$(BUILD_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-win32-$(TARGET_ARCH) \
 	| $(BUILD_OUTPUT_DIRECTORY) $(BUILD_TEMPORARY_DIRECTORY)
 	./scripts/build/electron-installer-nsis-win32.sh -n $(APPLICATION_NAME) -a $< -t $(BUILD_TEMPORARY_DIRECTORY) -o $@
+
 ifdef CODE_SIGN_CERTIFICATE
 ifdef CODE_SIGN_CERTIFICATE_PASSWORD
 	./scripts/build/electron-sign-exe-win32.sh -f $@ \
@@ -332,10 +399,13 @@ TARGETS = \
 	sanity-checks \
 	clean \
 	distclean \
-	package \
+	package-electron \
+	package-cli \
+	cli-develop \
 	electron-develop
 
-package: $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH)
+package-electron: $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH)
+package-cli: $(BUILD_DIRECTORY)/$(APPLICATION_NAME)-cli-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH)
 
 ifeq ($(TARGET_PLATFORM),darwin)
 electron-installer-app-zip: $(BUILD_OUTPUT_DIRECTORY)/$(APPLICATION_NAME)-$(APPLICATION_VERSION)-$(TARGET_PLATFORM)-$(TARGET_ARCH).zip
@@ -396,7 +466,7 @@ endif
 
 .PHONY: $(TARGETS)
 
-electron-develop:
+# Note that we need to remove `node_modules` every time.
 # Since we use an `npm-shrinkwrap.json` file, if you pull changes
 # that update a dependency and try to `npm install` directly, npm
 # will complain that your `node_modules` tree is not equal to what
@@ -405,6 +475,16 @@ electron-develop:
 # The `node_modules` directory also needs to be wiped out if you're
 # changing between target architectures, since compiled add-ons
 # will not work otherwise.
+
+cli-develop:
+	rm -rf node_modules
+	./scripts/build/dependencies-npm.sh \
+		-r "$(TARGET_ARCH)" \
+		-v "$(ELECTRON_VERSION)" \
+		-t node \
+		-s "$(TARGET_PLATFORM)"
+
+electron-develop:
 	rm -rf node_modules
 	./scripts/build/dependencies-npm.sh \
 		-r "$(TARGET_ARCH)" \

scripts/build/node-package-cli.sh
@@ -1,0 +1,82 @@
+#!/bin/bash
+
+###
+# Copyright 2016 resin.io
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+set -u
+set -e
+
+function usage() {
+  echo "Usage: $0"
+  echo ""
+  echo "Options"
+  echo ""
+  echo "    -n <application name>"
+  echo "    -e <application entry point (.js)>"
+  echo "    -l <node_modules directory>"
+  echo "    -r <architecture>"
+  echo "    -s <operating system (linux|darwin|win32)>"
+  echo "    -o <output directory>"
+  exit 1
+}
+
+ARGV_APPLICATION_NAME=""
+ARGV_ENTRY_POINT=""
+ARGV_NODE_MODULES=""
+ARGV_ARCHITECTURE=""
+ARGV_OPERATING_SYSTEM=""
+ARGV_OUTPUT=""
+
+while getopts ":n:e:l:r:s:o:" option; do
+  case $option in
+    n) ARGV_APPLICATION_NAME="$OPTARG" ;;
+    e) ARGV_ENTRY_POINT="$OPTARG" ;;
+    l) ARGV_NODE_MODULES="$OPTARG" ;;
+    r) ARGV_ARCHITECTURE="$OPTARG" ;;
+    s) ARGV_OPERATING_SYSTEM="$OPTARG" ;;
+    o) ARGV_OUTPUT="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
+if [ -z "$ARGV_APPLICATION_NAME" ] \
+  || [ -z "$ARGV_ENTRY_POINT" ] \
+  || [ -z "$ARGV_NODE_MODULES" ] \
+  || [ -z "$ARGV_ARCHITECTURE" ] \
+  || [ -z "$ARGV_OPERATING_SYSTEM" ] \
+  || [ -z "$ARGV_OUTPUT" ]; then
+  usage
+fi
+
+mkdir "$ARGV_OUTPUT"
+cp "$ARGV_ENTRY_POINT" "$ARGV_OUTPUT/index.js"
+
+./scripts/build/dependencies-npm-extract-addons.sh \
+  -d "$ARGV_NODE_MODULES" \
+  -o "$ARGV_OUTPUT/node_modules"
+
+APPLICATION_NAME_LOWERCASE="$(echo "$ARGV_APPLICATION_NAME" | tr '[:upper:]' '[:lower:]')"
+BINARY_LOCATION="$ARGV_OUTPUT/$APPLICATION_NAME_LOWERCASE"
+if [ "$ARGV_OPERATING_SYSTEM" == "win32" ]; then
+  BINARY_LOCATION="$BINARY_LOCATION.exe"
+fi
+
+./scripts/build/node-static-entry-point-download.sh \
+  -r "$ARGV_ARCHITECTURE" \
+  -v "1.0.1" \
+  -s "$ARGV_OPERATING_SYSTEM" \
+  -o "$BINARY_LOCATION"
+chmod +x "$BINARY_LOCATION"