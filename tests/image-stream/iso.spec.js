balena-io/etcher

commit b3b928ae4ff5da7cd21e033dbdf94963e0e81ddd

feat(image-stream): Support Apple Disk Images (#1257)
This adds support for flashing Apple Disk Images (.dmg)

Change-Type: minor

.gitattributes
@@ -30,3 +30,4 @@ Makefile text
 *.png binary
 *.xz binary
 *.zip binary
+*.dmg binary

lib/image-stream/utils.js
@@ -18,15 +18,16 @@
 
 const _ = require('lodash');
 const Bluebird = require('bluebird');
-const fs = Bluebird.promisifyAll(require('fs'));
-const archiveType = require('archive-type');
+const fileType = require('file-type');
+const mime = require('mime-types');
+const fs = require('fs');
 
 /**
  * @summary Get archive mime type
  * @function
  * @public
  *
- * @param {String} file - file path
+ * @param {String} filename - file path
  * @fulfil {String} - mime type
  * @returns {Promise}
  *
@@ -35,28 +36,27 @@ const archiveType = require('archive-type');
  *   console.log(mimeType);
  * });
  */
-exports.getArchiveMimeType = (file) => {
+exports.getArchiveMimeType = (filename) => {
 
-  // `archive-type` only needs the first 261 bytes
-  // See https://github.com/kevva/archive-type
-  const ARCHIVE_TYPE_IDENTIFICATION_BYTES_LENGTH = 261;
+  const mimeType = mime.lookup(filename);
 
-  return Bluebird.using(fs.openAsync(file, 'r').disposer((fileDescriptor) => {
+  if (mimeType) {
+    return Bluebird.resolve(mimeType);
+  }
+
+  const FILE_TYPE_ID_BYTES = 262;
+
+  return Bluebird.using(fs.openAsync(filename, 'r').disposer((fileDescriptor) => {
     return fs.closeAsync(fileDescriptor);
   }), (fileDescriptor) => {
     const BUFFER_START = 0;
-    const chunk = new Buffer(ARCHIVE_TYPE_IDENTIFICATION_BYTES_LENGTH);
+    const buffer = Buffer.alloc(FILE_TYPE_ID_BYTES);
 
-    return fs.readAsync(
-      fileDescriptor,
-      chunk,
-      BUFFER_START,
-      ARCHIVE_TYPE_IDENTIFICATION_BYTES_LENGTH,
-      null
-    ).then(() => {
-      return _.get(archiveType(chunk), [ 'mime' ], 'application/octet-stream');
+    return fs.readAsync(fileDescriptor, buffer, BUFFER_START, FILE_TYPE_ID_BYTES, null).then(() => {
+      return _.get(fileType(buffer), [ 'mime' ], 'application/octet-stream');
     });
   });
+
 };
 
 /**

tests/image-stream/utils.spec.js
@@ -26,44 +26,53 @@ describe('ImageStream: Utils', function() {
 
   describe('.getArchiveMimeType()', function() {
 
-    it('should resolve application/x-bzip2 for a bz2 archive', function(done) {
+    it('should resolve application/x-bzip2 for a bz2 archive', function() {
       const file = path.join(DATA_PATH, 'bz2', 'raspberrypi.img.bz2');
-      utils.getArchiveMimeType(file).then((type) => {
+      return utils.getArchiveMimeType(file).then((type) => {
         m.chai.expect(type).to.equal('application/x-bzip2');
-        done();
-      }).catch(done);
+      });
     });
 
-    it('should resolve application/x-xz for a xz archive', function(done) {
+    it('should resolve application/x-xz for a xz archive', function() {
       const file = path.join(DATA_PATH, 'xz', 'raspberrypi.img.xz');
-      utils.getArchiveMimeType(file).then((type) => {
+      return utils.getArchiveMimeType(file).then((type) => {
         m.chai.expect(type).to.equal('application/x-xz');
-        done();
-      }).catch(done);
+      });
     });
 
-    it('should resolve application/gzip for a gz archive', function(done) {
+    it('should resolve application/gzip for a gz archive', function() {
       const file = path.join(DATA_PATH, 'gz', 'raspberrypi.img.gz');
-      utils.getArchiveMimeType(file).then((type) => {
+      return utils.getArchiveMimeType(file).then((type) => {
         m.chai.expect(type).to.equal('application/gzip');
-        done();
-      }).catch(done);
+      });
     });
 
-    it('should resolve application/zip for a zip archive', function(done) {
+    it('should resolve application/zip for a zip archive', function() {
       const file = path.join(DATA_PATH, 'zip', 'zip-directory-rpi-only.zip');
-      utils.getArchiveMimeType(file).then((type) => {
+      return utils.getArchiveMimeType(file).then((type) => {
         m.chai.expect(type).to.equal('application/zip');
-        done();
-      }).catch(done);
+      });
     });
 
-    it('should resolve application/octet-stream for an uncompress image', function(done) {
+    it('should resolve application/octet-stream for an uncompressed image', function() {
       const file = path.join(DATA_PATH, 'images', 'raspberrypi.img');
-      utils.getArchiveMimeType(file).then((type) => {
+      return utils.getArchiveMimeType(file).then((type) => {
         m.chai.expect(type).to.equal('application/octet-stream');
-        done();
-      }).catch(done);
+      });
+    });
+
+    it('should resolve application/x-apple-diskimage for a compressed Apple disk image', function() {
+      const file = path.join(DATA_PATH, 'dmg', 'zlib-compressed.dmg');
+      return utils.getArchiveMimeType(file).then((type) => {
+        m.chai.expect(type).to.equal('application/x-apple-diskimage');
+      });
+    });
+
+    it('should resolve application/x-apple-diskimage for an uncompressed Apple disk image', function() {
+      const file = path.join(DATA_PATH, 'dmg', 'raw.dmg');
+      return utils.getArchiveMimeType(file).then((type) => {
+        m.chai.expect(type).to.equal('application/x-apple-diskimage');
+      });
     });
 
   });

lib/image-stream/handlers.js
@@ -23,6 +23,7 @@ const lzma = Bluebird.promisifyAll(require('lzma-native'));
 const zlib = require('zlib');
 const unbzip2Stream = require('unbzip2-stream');
 const gzip = require('./gzip');
+const udif = Bluebird.promisifyAll(require('udif'));
 const archive = require('./archive');
 const zipArchiveHooks = require('./archive-hooks/zip');
 
@@ -121,6 +122,35 @@ module.exports = {
           }
         },
         transform: lzma.createDecompressor()
+      };
+    });
+  },
+
+  /**
+   * @summary Handle Apple disk images (.dmg)
+   * @function
+   * @public
+   * @memberof handlers
+   *
+   * @param {String} file - file path
+   * @param {Object} options - options
+   * @param {Number} [options.size] - file size
+   *
+   * @fulfil {Object} - image metadata
+   * @returns {Promise}
+   */
+  'application/x-apple-diskimage': (file, options) => {
+    return udif.getUncompressedSizeAsync(file).then((size) => {
+      return {
+        stream: udif.createReadStream(file),
+        size: {
+          original: options.size,
+          final: {
+            estimation: false,
+            value: size
+          }
+        },
+        transform: new PassThroughStream()
       };
     });
   },

lib/image-stream/supported.js
@@ -38,6 +38,10 @@ module.exports = [
     type: 'compressed'
   },
   {
+    extension: 'dmg',
+    type: 'compressed'
+  },
+  {
     extension: 'img',
     type: 'image'
   },

tests/gui/models/supported-formats.spec.js
@@ -23,7 +23,7 @@ describe('Browser: SupportedFormats', function() {
 
       it('should return the supported compressed extensions', function() {
         const extensions = SupportedFormatsModel.getCompressedExtensions();
-        m.chai.expect(extensions).to.deep.equal([ 'gz', 'bz2', 'xz' ]);
+        m.chai.expect(extensions).to.deep.equal([ 'gz', 'bz2', 'xz', 'dmg' ]);
       });
 
     });

commit c3793c1a9e01a17be1de3abab35a1cc72ba3b6a6

fix(image-stream): interpret iso9660 as application/octet-stream
This is a regression caused by
https://github.com/resin-io/etcher/pull/1257, which introduced a new way
to detect mime types by using the `mime-types` module.

This module, contrary to `file-type`, will detect certain ISO files as
`application/x-iso9660-image`, which Etcher doesn't know how to handle,
and will therefore should at the user that the format is unsupported.

Change-Type: patch
Changelog-Entry: Don't interpret certain ISO images as unsupported.
Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

.gitattributes
@@ -27,6 +27,7 @@ Makefile text
 *.icns binary
 *.ico binary
 *.img binary
+*.iso binary
 *.png binary
 *.xz binary
 *.zip binary

lib/image-stream/utils.js
@@ -37,10 +37,14 @@ const fs = require('fs');
  * });
  */
 exports.getArchiveMimeType = (filename) => {
-
+  const MIME_TYPE_RAW_IMAGE = 'application/octet-stream';
   const mimeType = mime.lookup(filename);
 
   if (mimeType) {
+    if (mimeType === 'application/x-iso9660-image') {
+      return Bluebird.resolve(MIME_TYPE_RAW_IMAGE);
+    }
+
     return Bluebird.resolve(mimeType);
   }
 
@@ -53,7 +57,7 @@ exports.getArchiveMimeType = (filename) => {
     const buffer = Buffer.alloc(FILE_TYPE_ID_BYTES);
 
     return fs.readAsync(fileDescriptor, buffer, BUFFER_START, FILE_TYPE_ID_BYTES, null).then(() => {
-      return _.get(fileType(buffer), [ 'mime' ], 'application/octet-stream');
+      return _.get(fileType(buffer), [ 'mime' ], MIME_TYPE_RAW_IMAGE);
     });
   });
 

tests/image-stream/iso.spec.js
@@ -1,0 +1,63 @@
+/*
+ * Copyright 2016 resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const m = require('mochainon');
+const fs = require('fs');
+const path = require('path');
+const DATA_PATH = path.join(__dirname, 'data');
+const IMAGES_PATH = path.join(DATA_PATH, 'images');
+const imageStream = require('../../lib/image-stream/index');
+const tester = require('./tester');
+
+describe('ImageStream: ISO', function() {
+
+  this.timeout(20000);
+
+  describe('.getFromFilePath()', function() {
+
+    describe('given an iso image', function() {
+      tester.extractFromFilePath(
+        path.join(IMAGES_PATH, 'raspberrypi.iso'),
+        path.join(IMAGES_PATH, 'raspberrypi.iso'));
+    });
+
+  });
+
+  describe('.getImageMetadata()', function() {
+
+    it('should return the correct metadata', function() {
+      const image = path.join(IMAGES_PATH, 'raspberrypi.iso');
+      const expectedSize = fs.statSync(image).size;
+
+      return imageStream.getImageMetadata(image).then((metadata) => {
+        m.chai.expect(metadata).to.deep.equal({
+          path: image,
+          size: {
+            original: expectedSize,
+            final: {
+              estimation: false,
+              value: expectedSize
+            }
+          }
+        });
+      });
+    });
+
+  });
+
+});

tests/image-stream/utils.spec.js
@@ -61,6 +61,13 @@ describe('ImageStream: Utils', function() {
       });
     });
 
+    it('should resolve application/octet-stream for an uncompressed iso', function() {
+      const file = path.join(DATA_PATH, 'images', 'raspberrypi.iso');
+      return utils.getArchiveMimeType(file).then((type) => {
+        m.chai.expect(type).to.equal('application/octet-stream');
+      });
+    });
+
     it('should resolve application/x-apple-diskimage for a compressed Apple disk image', function() {
       const file = path.join(DATA_PATH, 'dmg', 'zlib-compressed.dmg');
       return utils.getArchiveMimeType(file).then((type) => {