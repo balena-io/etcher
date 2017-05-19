ec571752f5bb59b116814b761fed6208a0e550f8
Shou <x@shou.io>
2017-05-19 14:26:54 +0100

    refactor(GUI): rewrite svg-icon directive in react
    We change from using an Angular directive to using React through
    react2angular in the SVGIcon module.
    
    Changelog-Entry: Use React instead of Angular for the SVGIcon directive.

--- tests/gui/components/svg-icon.spec.js
+++ tests/gui/components/svg-icon.spec.js
@@ -22,18 +22,10 @@ describe('Browser: SVGIcon', function() {
       $compile = _$compile_;
       $rootScope = _$rootScope_;
 
-      // Workaround `Unexpected request: GET template.html. No more request expected` error.
-      // See http://stackoverflow.com/a/29437480/1641422
-      const templatePath = './components/svg-icon/templates/svg-icon.tpl.html';
-      const template = fs.readFileSync(path.resolve('lib', 'gui', templatePath), {
-        encoding: 'utf8'
-      });
-      $templateCache.put(templatePath, template);
-
     }));
 
     it('should inline the svg contents in the element', function() {
-      const icon = '../../../../../lib/gui/assets/etcher.svg';
+      const icon = '../../../../lib/gui/assets/etcher.svg';
       let iconContents = _.split(fs.readFileSync(path.join(__dirname, '../../../lib/gui/assets/etcher.svg'), {
         encoding: 'utf8'
       }), /\r?\n/);
@@ -43,16 +35,16 @@ describe('Browser: SVGIcon', function() {
       iconContents[0] = `<!--${iconContents[0].slice(1, iconContents[0].length - 1)}-->`;
       iconContents = iconContents.join('\n');
 
-      const element = $compile(`<svg-icon path="${icon}">Resin.io</svg-icon>`)($rootScope);
+      const element = $compile(`<svg-icon path="'${icon}'">Resin.io</svg-icon>`)($rootScope);
       $rootScope.$digest();
-      m.chai.expect(element.html()).to.equal(iconContents);
+      m.chai.expect(element.children().html()).to.equal(iconContents);
     });
 
     it('should inline raw svg contents in the element', function() {
       const svg = '<svg><text>Raspbian</text></svg>';
-      const element = $compile(`<svg-icon path="${svg}"></svg-icon>`)($rootScope);
+      const element = $compile(`<svg-icon path="'${svg}'"></svg-icon>`)($rootScope);
       $rootScope.$digest();
-      m.chai.expect(element.html()).to.equal(svg);
+      m.chai.expect(element.children().html()).to.equal(svg);
     });
 
     it('should react to external updates', function() {
@@ -62,37 +54,37 @@ describe('Browser: SVGIcon', function() {
         return `<svg><text>${scope.name}</text></svg>`;
       };
 
-      const element = $compile('<svg-icon path="{{ getSvg() }}"></svg-icon>')(scope);
+      const element = $compile('<svg-icon path="getSvg()"></svg-icon>')(scope);
       $rootScope.$digest();
-      m.chai.expect(element.html()).to.equal('<svg><text>Raspbian</text></svg>');
+      m.chai.expect(element.children().html()).to.equal('<svg><text>Raspbian</text></svg>');
 
       scope.name = 'Resin.io';
       $rootScope.$digest();
-      m.chai.expect(element.html()).to.equal('<svg><text>Resin.io</text></svg>');
+      m.chai.expect(element.children().html()).to.equal('<svg><text>Resin.io</text></svg>');
     });
 
     it('should default the size to 40x40 pixels', function() {
-      const icon = '../../../../../lib/gui/assets/etcher.svg';
-      const element = $compile(`<svg-icon path="${icon}">Resin.io</svg-icon>`)($rootScope);
+      const icon = '../../../../lib/gui/assets/etcher.svg';
+      const element = $compile(`<svg-icon path="'${icon}'">Resin.io</svg-icon>`)($rootScope);
       $rootScope.$digest();
-      m.chai.expect(element.css('width')).to.equal('40px');
-      m.chai.expect(element.css('height')).to.equal('40px');
+      m.chai.expect(element.children().css('width')).to.equal('40px');
+      m.chai.expect(element.children().css('height')).to.equal('40px');
     });
 
     it('should be able to set a custom width', function() {
-      const icon = '../../../../../lib/gui/assets/etcher.svg';
-      const element = $compile(`<svg-icon path="${icon}" width="20px">Resin.io</svg-icon>`)($rootScope);
+      const icon = '../../../../lib/gui/assets/etcher.svg';
+      const element = $compile(`<svg-icon path="'${icon}'" width="'20px'">Resin.io</svg-icon>`)($rootScope);
       $rootScope.$digest();
-      m.chai.expect(element.css('width')).to.equal('20px');
-      m.chai.expect(element.css('height')).to.equal('40px');
+      m.chai.expect(element.children().css('width')).to.equal('20px');
+      m.chai.expect(element.children().css('height')).to.equal('40px');
     });
 
     it('should be able to set a custom height', function() {
-      const icon = '../../../../../lib/gui/assets/etcher.svg';
-      const element = $compile(`<svg-icon path="${icon}" height="20px">Resin.io</svg-icon>`)($rootScope);
+      const icon = '../../../../lib/gui/assets/etcher.svg';
+      const element = $compile(`<svg-icon path="'${icon}'" height="'20px'">Resin.io</svg-icon>`)($rootScope);
       $rootScope.$digest();
-      m.chai.expect(element.css('width')).to.equal('40px');
-      m.chai.expect(element.css('height')).to.equal('20px');
+      m.chai.expect(element.children().css('width')).to.equal('40px');
+      m.chai.expect(element.children().css('height')).to.equal('20px');
     });
 
   });
