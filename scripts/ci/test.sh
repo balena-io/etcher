a12abc2a6cf6c2b5b18c416b6bc2750d97c9db10
Juan Cruz Viotti <jviotti@openmailbox.org>
2017-05-22 09:20:28 -0400

    chore: unify CI scripts (#1434)
    Currently we have almost equal CI scripts implemented in bash (for
    Travis), and batch (for Appveyor). This commit takes advantage of MinGW
    bash to unify all the scripts as bash scripts.
    
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -19,20 +19,40 @@
 set -e
 set -u
 
-if [ -z "$TRAVIS_OS_NAME" ]; then
-  echo "This script is only meant to run in Travis CI" 1>&2
+function usage() {
+  echo "Usage: $0"
+  echo ""
+  echo "Options"
+  echo ""
+  echo "    -o <operating system>"
+  echo "    -r <architecture>"
   exit 1
+}
+
+ARGV_OPERATING_SYSTEM=""
+ARGV_ARCHITECTURE=""
+
+while getopts ":o:r:" option; do
+  case $option in
+    o) ARGV_OPERATING_SYSTEM=$OPTARG ;;
+    r) ARGV_ARCHITECTURE=$OPTARG ;;
+    *) usage ;;
+  esac
+done
+
+if [ -z "$ARGV_OPERATING_SYSTEM" ] || [ -z "$ARGV_ARCHITECTURE" ]; then
+  usage
 fi
 
-./scripts/build/check-dependency.sh npm
-./scripts/build/check-dependency.sh make
-
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+if [ "$ARGV_OPERATING_SYSTEM" == "linux" ]; then
   ./scripts/build/docker/run-command.sh \
-    -r "$TARGET_ARCH" \
+    -r "$ARGV_ARCHITECTURE" \
     -s "$(pwd)" \
     -c 'make sanity-checks && xvfb-run --server-args=$XVFB_ARGS npm test'
 else
+  ./scripts/build/check-dependency.sh make
+  ./scripts/build/check-dependency.sh npm
+
   make sanity-checks
   npm test
 fi
