d355dd0a8790a003edf061e893b0d0ccaa81deb2
Juan Cruz Viotti <jviotti@openmailbox.org>
2017-05-29 20:57:27 -0400

    chore: build armhf on GNU/Linux
    This commit makes use of the `resin/armv7hf-debian` Docker image to
    test and generate armhf builds.
    
    We needed to add a slash before `build` in `.gitignore` given that git
    was refusing to include any changes on `scripts/build` otherwise.
    
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- scripts/build/docker/compile-template.js
+++ scripts/build/docker/compile-template.js
@@ -36,6 +36,10 @@ _.each([
   {
     architecture: 'x86_64',
     image: 'ubuntu:12.04'
+  },
+  {
+    architecture: 'armv7hf',
+    image: 'resin/armv7hf-debian:jessie'
   }
 ], (options) => {
   const result = _.template(template)(options)
