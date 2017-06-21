b380537ad751db2d9603491323a207f0ba7e9773
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-06-21 17:30:04 +0200

    chore(scripts): Don't shrinkwrap with --dev

--- scripts/ci/ensure-staged-shrinkwrap.sh
+++ scripts/ci/ensure-staged-shrinkwrap.sh
@@ -24,12 +24,12 @@ echo "npm version: $(npm --version)"
 
 SHRINKWRAP_FILE=npm-shrinkwrap.json
 
-npm shrinkwrap --dev
+npm shrinkwrap
 
 if [[ -n $(git status -s "$SHRINKWRAP_FILE") ]]; then
   echo "There are unstaged $SHRINKWRAP_FILE changes. Please commit the result of:" 1>&2
   echo ""
-  echo "    npm shrinkwrap --dev" 1>&2
+  echo "    npm shrinkwrap" 1>&2
   echo ""
   git --no-pager diff "$SHRINKWRAP_FILE"
   exit 1
