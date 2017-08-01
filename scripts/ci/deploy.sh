de62b2e65c04c426eb9b92bbb8d6c1653ca368e3
Juan Cruz Viotti <jv@jviotti.com>
2017-08-01 10:29:09 -0400

    chore: publish development Bintray packages (#1611)
    This commit includes several changes to adapt the CI configuration files
    and Bintray publish script to perform development deployments.
    
    - Move our Bintray details to the Makefile
    - Deploy to a new Bintray component if `RELEASE_TYPE` is `snapshot`
    - Call `publish-bintray-debian` and `publish-bintray-redhat` in the CI
      deployment script
    - Call the Bintray deployment scripts for RPMs
    
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -48,8 +48,8 @@ if [ "$ARGV_OPERATING_SYSTEM" == "linux" ]; then
   ./scripts/build/docker/run-command.sh \
     -r "$ARGV_ARCHITECTURE" \
     -s "$(pwd)" \
-    -c "make publish-aws-s3"
+    -c "make installers-all"
 else
   ./scripts/build/check-dependency.sh make
-  make publish-aws-s3
+  make installers-all
 fi
