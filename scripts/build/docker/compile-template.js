d4fabbde16fd0cd5fb6e6d92f2a85c4b34fe2dcd
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-10-24 14:33:10 +0200

    chore(scripts): Update Dockerfiles' base to Ubuntu 16.04

--- scripts/build/docker/compile-template.js
+++ scripts/build/docker/compile-template.js
@@ -31,11 +31,11 @@ const template = fs.readFileSync(path.join(currentDirectory, 'Dockerfile.templat
 _.each([
   {
     architecture: 'i686',
-    image: 'erwinchang/ubuntu-12.04-32bit-build'
+    image: 'erwinchang/ubuntu-16.04-32bit-build'
   },
   {
     architecture: 'x86_64',
-    image: 'ubuntu:12.04'
+    image: 'ubuntu:16.04'
   },
   {
     architecture: 'armv7hf',
