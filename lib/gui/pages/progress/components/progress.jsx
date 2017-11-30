fd42a7c8f8b0e9dd9530d040fb37cf0d6dd94d0d
Benedict Aas <x@shou.io>
2017-11-30 23:28:00 +0000

    styles close

--- lib/gui/pages/progress/components/progress.jsx
+++ lib/gui/pages/progress/components/progress.jsx
@@ -20,6 +20,132 @@ const React = require('react')
 const propTypes = require('prop-types')
 const flashState = require('../../../../shared/models/flash-state')
 const styled = require('styled-components').default
+const prettyBytes = require('pretty-bytes')
+const path = require('path')
+
+const ProgressContainer = styled.div`
+  background-color: gray;
+  border-radius: ${props => props.height};
+  width: ${props => props.width };
+  height: ${props => props.height };
+  overflow: hidden;
+`
+
+const ProgressFill = styled.div`
+  background-color: ${props => props.color};
+  height: ${props => props.height };
+  width: ${props => props.value}%;
+  transition: width 0.2s;
+`
+
+class ProgressBar extends React.PureComponent {
+  render() {
+    return (
+      <ProgressContainer width={ this.props.width } height={ this.props.height }>
+        <ProgressFill
+          value={ this.props.value }
+          height={ this.props.height }
+          color={ this.props.color }
+        />
+      </ProgressContainer>
+    )
+  }
+}
+
+const StyledTotalProgressBarWrapper = styled.div`
+  width: 33%;
+`
+
+const StyledTotalProgressBarLabel = styled.div`
+  display: flex;
+  justify-content: center;
+  color: white;
+  font-weight: bold;
+  margin-bottom: 7px;
+`
+
+const StyledTotalProgressBarSubtitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: gray;
+  margin-top: 5px;
+`
+
+class TitleWithProgressBar extends React.PureComponent {
+  render() {
+    return (
+      <StyledTotalProgressBarWrapper>
+        <StyledTotalProgressBarLabel>{ this.props.label }</StyledTotalProgressBarLabel>
+        <ProgressBar
+          value={ this.props.percent }
+          width={ '220px' }
+          height={ '7px' }
+          color={ this.props.color }
+        />
+        <StyledTotalProgressBarSubtitle>
+          <div>{ this.props.leftSubtitle }</div>
+          <div>{ this.props.rightSubtitle }</div>
+        </StyledTotalProgressBarSubtitle>
+      </StyledTotalProgressBarWrapper>
+    )
+  }
+}
+
+const Title = styled.span`
+  color: white;
+  font-weight: bold;
+`
+
+const Dim = styled.span`
+  color: gray;
+`
+
+const DriveStatusWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 33%;
+`
+
+class DriveStatus extends React.PureComponent {
+  render() {
+    return (
+      <DriveStatusWrapper>
+        <Title>{ this.props.label }</Title> <Dim>({ this.props.quantity })</Dim>
+      </DriveStatusWrapper>
+    )
+  }
+}
+
+const StyledProgressGaugeSidesWrapper = styled.div`
+  border: 7px solid gray;
+  border-radius: 50%;
+  width: ${props => props.size};
+  height: ${props => props.size};
+  position: relative;
+`
+
+const StyledProgressGaugeSides = styled.div`
+  width: calc(100% + 14px);
+  height: calc(100% + 14px);
+  margin-top: -7px;
+  margin-left: -7px;
+  border: 7px solid #ff912f;
+  border-radius: 50%;
+  position: absolute;
+  clip-path: inset(0 50% 0 0);
+  transition: transform 0.2s;
+`
+
+const StyledProgressGaugeLabel = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
 
 class ProgressGauge extends React.PureComponent {
   constructor(props) {
@@ -31,65 +157,31 @@ class ProgressGauge extends React.PureComponent {
   }
 
   render() {
-    const borderSize = this.props.size / 2
-    console.log(this.props.size)
-
-    const wrapStyles = {
-      border: `${borderSize}px solid gray`,
-      borderRadius: '50%',
-      width: Math.max(this.props.size, this.props.minSize) + 'vh',
-      height: Math.max(this.props.size, this.props.minSize) + 'vh',
-      position: 'relative'
-    }
-
     const clipStyles = {
       width: '100%',
       height: '100%',
     }
 
-    const sideStyles = {
-      width: `calc(100% + ${borderSize * 2}px)`,
-      height: `calc(100% + ${borderSize * 2}px)`,
-      marginTop: `-${borderSize}px`,
-      marginLeft: `-${borderSize}px`,
-      border: `${borderSize}px solid #ff912f`,
-      borderRadius: '50%',
-      position: 'absolute',
-      clipPath: 'inset(0 50% 0 0)',
-      transition: 'all 0.2s'
-    }
-
     const degreeFactor = 3.6
     const progressDegrees = Math.round(this.props.percentage * degreeFactor)
 
-    const leftStyles = _.assign({}, sideStyles, {
+    const leftStyles = {
       transform: `rotate(${Math.min(progressDegrees, 180)}deg)`
-    })
+    }
 
-    const rightStyles = _.assign({}, sideStyles, {
+    const rightStyles = {
       transform: `rotate(${progressDegrees < 180 ? 0 : progressDegrees}deg)`,
       borderColor: progressDegrees < 180 ? 'gray' : '#ff912f'
-    })
-
-    const labelStyles = {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      top: '0',
-      left: '0',
-      width: '100%',
-      height: '100%'
     }
 
     return (
-      <div style={ wrapStyles }>
+      <StyledProgressGaugeSidesWrapper size={ this.props.size } minSize={ this.props.minSize }>
         <div style={ clipStyles }>
-          <div style={ leftStyles }></div>
-          <div style={ rightStyles }></div>
+          <StyledProgressGaugeSides style={ leftStyles } />
+          <StyledProgressGaugeSides style={ rightStyles } />
         </div>
-        <div style={ labelStyles }>{ this.props.children }</div>
-      </div>
+        <StyledProgressGaugeLabel>{ this.props.children }</StyledProgressGaugeLabel>
+      </StyledProgressGaugeSidesWrapper>
     )
   }
 }
@@ -136,13 +228,21 @@ class ProgressMetadata extends React.PureComponent {
   }
 }
 
-const StyledProgressPageWrapper = styled.div`
+const StyledProgressGaugesWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   align-items: center;
   height: 300px;
+`
+
+const StyledProgressPageWrapper = styled.div`
   margin-top: -50px;
+`
+
+const StyledProgressHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
 `
 
 class ProgressPage extends React.PureComponent {
@@ -155,40 +255,86 @@ class ProgressPage extends React.PureComponent {
   }
 
   render() {
-    const statesLength = (this.props.states || []).length || 1
-    const size = Math.round(60 / statesLength)
-
-    const lengthSizes = [
-      100, 50, 33, 50, 33, 33, 25, 25, 33, 25, 25, 25
+    const size = '80px'
+    const sizes = [
+      0, 100, 50, 33, 50, 33, 33, 25, 25
     ]
-    const minSize = lengthSizes[statesLength]
+    const minSize = sizes[_.get(this.props.states, [ 'length' ], 0)] + '%'
+
+    // TODO move elsewhere in codebase
+    const stateLabels = {
+      write: 'Writing',
+      check: 'Verifying',
+      backup: 'Backing up'
+    }
 
     const gauges = this.props.states.map((state) => {
-      // TODO move elsewhere in codebase
-      const stateLabels = {
-        write: 'Writing',
-        check: 'Verifying',
-        backup: 'Backing up'
-      }
+      const isDone = state.percentage === 100
+
+      const checkmarkElement = (
+        <span className="tick tick--success" />
+      )
+
+      const percentageText = (
+        <StyledPercentageText size={ size / 3 }>
+          { `${state.percentage}%` }
+        </StyledPercentageText>
+      )
+
+      const centerPiece = isDone ? checkmarkElement : percentageText
+
+      const { description } = availableDrives.findDriveByDevice(state.device) || {}
 
       return (
         <StyledProgressGaugeWrapper>
           <ProgressGauge percentage={ state.percentage } size={ size } minSize={ minSize }>
-            <StyledPercentageText size={ size / 3 }>
-              { `${state.percentage}%` }
-            </StyledPercentageText>
+            { centerPiece }
           </ProgressGauge>
           <ProgressMetadata
-            title={ state.device }
+            title={ description }
             subtitle={ `${stateLabels[state.type]} - ${state.speed} MB/s` }>
           </ProgressMetadata>
         </StyledProgressGaugeWrapper>
       )
     })
 
+    const totalState = flashState.getAverageFlashState()
+    const { percentage } = totalState
+    const totalSpeed = prettyBytes(totalState.speed) + '/s'
+    const totalEta = `ETA: ${Math.round(totalState.eta * 100) / 100}s`
+    const isTotalDone = percentage === 100
+    const totalLabel = isTotalDone ? 'Done' : stateLabels[totalState.type] + '...'
+
+    const source = selectionState.getImage()
+
+    const drives = selectionState.getSelectedDrives()
+    const driveLabel = drives.length === 1 ? drives[0].description : 'Multiple drives'
+
     return (
       <StyledProgressPageWrapper>
-        { gauges }
+        <StyledProgressHeader>
+          <TitleWithProgressBar
+            leftSubtitle={ path.basename(_.get(source, [ 'path' ], '')) }
+            rightSubtitle={ '' }
+            percent={ 100 }
+            label={ 'Source' }
+            color={ '#5fb835' }
+          />
+          <DriveStatus
+            label={ driveLabel }
+            quantity={ drives.length }
+          />
+          <TitleWithProgressBar
+            leftSubtitle={ totalSpeed }
+            rightSubtitle={ totalEta }
+            percent={ percentage }
+            label={ totalLabel }
+            color={ isTotalDone ? '#5fb835' : '#ff912f' }
+          />
+        </StyledProgressHeader>
+        <StyledProgressGaugesWrapper>
+          { gauges }
+        </StyledProgressGaugesWrapper>
       </StyledProgressPageWrapper>
     )
   }
