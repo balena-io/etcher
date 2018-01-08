d172d564bbac208ee863b84a7fae4d6f65c78831
Benedict Aas <x@shou.io>
2018-01-08 16:36:04 +0000

    feat: test that ipc verbosity is off
    We test that `ipc.config.silent` is set to `true` so that it doesn't
    spam `stdout` for users.
    
    Changelog-Entry: Test that IPC verbosity is off
    Change-Type: patch

---
+++ tests/gui/modules/image-writer.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict'
+
+const m = require('mochainon')
+const ipc = require('node-ipc')
+const imageWriter = require('../../../lib/gui/modules/image-writer')
+
+describe('Browser: imageWriter', function () {
+  it('should have the ipc config set to silent', function () {
+    m.chai.expect(ipc.config.silent).to.be.true
+  })
+})
