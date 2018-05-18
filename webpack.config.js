2192536bceaaeb2684a20b3b1b2f82129e000657
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-05-18 18:54:12 +0200

    chore(webpack): Reduce build log verbosity
    Travis CI is complaining about 10k+ lines of log output again.
    
    Change-Type: patch

--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,7 @@ module.exports = {
   },
   plugins: [
     new SimpleProgressWebpackPlugin({
-      format: process.env.WEBPACK_PROGRESS || 'verbose'
+      format: process.env.WEBPACK_PROGRESS || 'compact'
     })
   ]
 }
