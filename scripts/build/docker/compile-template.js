d73469f18d7e0fef0290b0b14de9eb31165d3761
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-07-04 20:27:51 +0200

    fix(docker): Update 14.04 32bit base image name

--- scripts/build/docker/compile-template.js
+++ scripts/build/docker/compile-template.js
@@ -31,7 +31,7 @@ const template = fs.readFileSync(path.join(currentDirectory, 'Dockerfile.templat
 _.each([
   {
     architecture: 'i686',
-    image: 'erwinchang/ubuntu-14.04-32bit-build'
+    image: 'erwinchang/ubuntu-14.04-32bit'
   },
   {
     architecture: 'x86_64',
