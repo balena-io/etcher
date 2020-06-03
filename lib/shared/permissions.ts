b7e82f7694989dd525eacb98b4589048d846848b
Alexis Svinartchouk <alexis@resin.io>
2020-06-03 20:00:46 +0200

    Fix sudo-prompt promisification
    Changelog-entry: Fix sudo-prompt promisification
    Change-type: patch

--- lib/shared/permissions.ts
+++ lib/shared/permissions.ts
@@ -29,7 +29,10 @@ import { tmpFileDisposer } from './utils';
 
 const execAsync = promisify(childProcess.exec);
 const execFileAsync = promisify(childProcess.execFile);
-const sudoExecAsync = promisify(sudoPrompt.exec);
+// sudo-prompt's exec callback is function(error, stdout, stderr) so we need multiArgs
+const sudoExecAsync = Bluebird.promisify(sudoPrompt.exec, {
+	multiArgs: true,
+}) as (cmd: string, options: any) => Bluebird<[string, string]>;
 
 /**
  * @summary The user id of the UNIX "superuser"
