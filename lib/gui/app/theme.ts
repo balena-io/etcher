e74dc9eb6002202e392cd55b841b0ed4be777fa4
Lorenzo Alberto Maria Ambrosi <lorenzothunder.ambrosi@gmail.com>
2020-09-18 09:39:42 +0200

    Update rendition to v18.8.3
    Change-type: patch
    Signed-off-by: Lorenzo Alberto Maria Ambrosi <lorenzothunder.ambrosi@gmail.com>

--- lib/gui/app/theme.ts
+++ lib/gui/app/theme.ts
@@ -1,18 +1,21 @@
 /*
- * Copyright 2018 balena.io
- *
- * Licensed under the Apache License, Version 2.0 (the "License"),
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2018 balena.io
+*
+* Licensed under the Apache License, Version 2.0 (the "License"),
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import * as _ from "lodash";
+import { Theme } from "rendition";
 
 export const colors = {
 	dark: {
@@ -67,9 +70,12 @@ export const colors = {
 
 const font = 'SourceSansPro';
 
-export const theme = {
+export const theme = _.merge({}, Theme, {
 	colors,
 	font,
+	header: {
+		height: '40px',
+	},
 	global: {
 		font: {
 			family: font,
@@ -109,4 +115,4 @@ export const theme = {
 			}
 		`,
 	},
-};
+});
