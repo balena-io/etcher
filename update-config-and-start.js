eb0602a5f144fecaaa06985e7373a63f48a61c23
Alexis Svinartchouk <alexis@resin.io>
2020-11-10 13:30:15 +0100

    Disable success banner
    Change-type: patch

--- update-config-and-start.js
+++ update-config-and-start.js
@@ -23,6 +23,7 @@ const db = {
 			'/dev/mmcblk0boot1',
 		],
 		featuredProjectEndpoint: 'nothing://',
+		successBannerURL: '',
 		fullscreen: true,
 		ledsOrder: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
 	},
