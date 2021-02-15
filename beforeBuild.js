b206483c7cf37ef9865bc242b4053f6a5cc7cdec
Alexis Svinartchouk <alexis@resin.io>
2021-02-15 15:02:18 +0100

    Update scripts to build universal mac dmgs on the ci
    Change-type: patch

--- beforeBuild.js
+++ beforeBuild.js
@@ -6,21 +6,22 @@ const process = require('process');
 
 // Rebuild native modules for ia32 and run webpack again for the ia32 part of windows packages
 exports.default = function(context) {
-        if (['windows', 'mac'].includes(context.platform.name)) {
-                cp.execFileSync(
-                        'npx',
-                        ['electron-rebuild', '--types', 'dev', '--arch', context.arch],
-                );
+	if (['windows', 'mac'].includes(context.platform.name)) {
+		const run = context.platform.name === 'windows' ? 'sh' : 'node';
+		cp.execFileSync(
+			run,
+			['node_modules/.bin/electron-rebuild', '--types', 'dev', '--arch', context.arch],
+		);
 		rimraf.sync('generated');
-                cp.execFileSync(
-                        'npx',
-                        ['webpack'],
+		cp.execFileSync(
+			run,
+			['node_modules/.bin/webpack'],
 			{
 				env: {
 					...process.env,
 					npm_config_target_arch: context.arch,
 				},
 			},
-                );
-        }
+		);
+	}
 }
