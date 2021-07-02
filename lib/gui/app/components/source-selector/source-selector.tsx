d1a3f1cb88ff38f804caa9289d3205b09666c1e6
JSReds <andrea.rosci90@gmail.com>
2021-07-02 14:22:03 +0200

    Add possibile authorization as a query param
    Change-type: patch
    Signed-off-by: Andrea Rosci <andrear@balena.io>

--- lib/gui/app/components/source-selector/source-selector.tsx
+++ lib/gui/app/components/source-selector/source-selector.tsx
@@ -61,6 +61,7 @@ import ImageSvg from '../../../assets/image.svg';
 import SrcSvg from '../../../assets/src.svg';
 import { DriveSelector } from '../drive-selector/drive-selector';
 import { DrivelistDrive } from '../../../../shared/drive-constraints';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const recentUrlImagesKey = 'recentUrlImages';
 
@@ -334,12 +335,32 @@ export class SourceSelector extends React.Component<
 			analytics.logException(error);
 		}
 
+		if (this.isJson(decodeURIComponent(selected))) {
+			const config: AxiosRequestConfig = JSON.parse(
+				decodeURIComponent(selected),
+			);
+			return new sourceDestination.Http({
+				url: config.url!,
+				axiosInstance: axios.create(_.omit(config, ['url'])),
+			});
+		}
+
 		if (SourceType === sourceDestination.File) {
 			return new sourceDestination.File({
 				path: selected,
 			});
 		}
+
 		return new sourceDestination.Http({ url: selected });
+	}
+
+	public isJson(jsonString: string) {
+		try {
+			JSON.parse(jsonString);
+		} catch (e) {
+			return false;
+		}
+		return true;
 	}
 
 	private reselectSource() {
