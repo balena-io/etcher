db1bf7e4882bc3f1eaaaf9b7bbf247539be527d8
r-q <scrisqiu@hotmail.com>
2022-07-19 23:33:42 +0800

    feat: make i18n and add Chinese support
    - make i18n using i18next
    - add Chinese (Simplified) support

--- lib/gui/app/renderer.ts
+++ lib/gui/app/renderer.ts
@@ -1,5 +1,11 @@
 // @ts-nocheck
 import { main } from './app';
+import './i18n';
+import { ipcRenderer } from 'electron';
+
+let lang = Intl.DateTimeFormat().resolvedOptions().locale;
+lang = lang.substr(0, 2);
+ipcRenderer.send('change-lng', lang);
 
 if (module.hot) {
 	module.hot.accept('./app', () => {
