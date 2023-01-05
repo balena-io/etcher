19df114a099cefa1595db7c7531967c5006ce438
ab77 <anton@balena.io>
2023-01-05 10:18:11 -0800

    remove win-arm64 experiments

--- scripts/ci/postinstall.sh
+++ scripts/ci/postinstall.sh
@@ -7,18 +7,6 @@ set -ea
 # use specified or fallback to platform architecture
 arch="${arch:-$(node -e 'console.log(os.arch())')}"
 
-# FIXME: remove when lzma-native ships with win-arm64 suport
-# https://github.com/addaleax/lzma-native/issues/132
-pushd node_modules/lzma-native
-# https://docs.npmjs.com/cli/v9/using-npm/config#environment-variables
-NPM_CONFIG_arch="${arch}" \
-  NPM_CONFIG_target_arch="${NPM_CONFIG_arch}" npm i
-
-# https://github.com/prebuild/prebuildify#options
-# https://www.npmjs.com/package/node-gyp-build
-PREBUILD_ARCH="${NPM_CONFIG_arch}" npm run prebuild
-popd
-
 # FIXME: (re)build for the latest version of electron, since v13.x uses ancient node-gyp
 # https://releases.electronjs.org/releases/stable
 electron_version="${ELECTRON_VERSION:-$(npm view electron --json | jq -r '.version')}"
--- scripts/ci/preinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ea
-
-[[ "$VERBOSE" =~ on|On|Yes|yes|true|True ]] && set -x
-
-# FIXME: remove when lzma-native ships with win-arm64 suport
-# https://github.com/addaleax/lzma-native/issues/132
-find patches -type f | xargs cat && npx patch-package
