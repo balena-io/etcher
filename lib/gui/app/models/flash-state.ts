196fd8ae24de2a23ebaeae736c6ca41007162fa1
Edwin Joassart <edwin@blck.coffee>
2024-01-26 17:54:29 +0100

    patch: remove screensaver error when not on etcher-pro

--- lib/gui/app/models/flash-state.ts
+++ lib/gui/app/models/flash-state.ts
@@ -47,13 +47,7 @@ export function isFlashing(): boolean {
  */
 export function setFlashingFlag() {
 	// see https://github.com/balenablocks/balena-electron-env/blob/4fce9c461f294d4a768db8f247eea6f75d7b08b0/README.md#remote-methods
-	try {
-		electron.ipcRenderer.invoke('disable-screensaver');
-	} catch (error) {
-		console.log(
-			"Can't disable-screensaver, we're probably not running on a balena-electron env",
-		);
-	}
+	electron.ipcRenderer.send('disable-screensaver');
 	store.dispatch({
 		type: Actions.SET_FLASHING_FLAG,
 		data: {},
@@ -76,7 +70,8 @@ export function unsetFlashingFlag(results: {
 		data: results,
 	});
 	// see https://github.com/balenablocks/balena-electron-env/blob/4fce9c461f294d4a768db8f247eea6f75d7b08b0/README.md#remote-methods
-	electron.ipcRenderer.invoke('enable-screensaver');
+
+	electron.ipcRenderer.send('enable-screensaver');
 }
 
 export function setDevicePaths(devicePaths: string[]) {
